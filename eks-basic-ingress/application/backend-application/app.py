ffec7bc538b8a02db8d6bad9d065faf39811c2c4
kgh2133 <the2133@naver.com>
2022-08-18 20:02:03 +0900

    'add'

---
+++ eks-basic-ingress/application/backend-application/app.py
@@ -0,0 +1,27 @@
+from flask import *
+
+app = Flask(__name__)
+v1 = Blueprint('v1', __name__, url_prefix='/v1')
+
+@v1.route('/')
+def index():
+    return render_template_string('hello this is index page.')
+
+@v1.route('/health')
+def health():
+    return jsonify({'status':'up'})
+
+@v1.route('/<username>')
+def username(username):
+    return jsonify({'username':f'{username}', 'message':f'hello! {username}'})
+
+@v1.route('/<username>/<command>')
+def username_command(username, command):
+    return jsonify({'username':f'{username}', 'message':f'hello! {username}'})
+
+@v1.route('/ip')
+def ip():
+    return jsonify({'user-ip':request.remote_addr})
+
+app.register_blueprint(v1)
+app.run(host='0.0.0.0', port=8080)
